651d2f03d7a9cb097e8eb5113a08f3c63b041a3e
kelgrei <98072994+kelgrei@users.noreply.github.com>
2022-05-19 01:27:50 -0300

    Create Algoritmo.js

---
+++ Algoritmo.js
@@ -0,0 +1,19 @@
+Algoritmo "Calcular Média de notas"
+
+Var
+Nota1, nota2, media:Real
+
+Inicio
+Escreva("Informa a nota 1:")
+Leia(Nota1)
+Escreva("Informa a nota 2:")
+Leia(Nota2)
+media <- (Nota1 + Nota2)/2
+Escreva(media)
+Se (media >- 6) então
+    Escreva("Aprovado")
+Senão
+    Escreva("Reprovado")
+fimse
+
+Fimalgoritmo
