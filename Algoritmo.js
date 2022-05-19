63c4b20608a832e8748969bd48fd7f9991a113c4
kelgrei <98072994+kelgrei@users.noreply.github.com>
2022-05-19 01:44:38 -0300

    Update Algoritmo.js

--- Algoritmo.js
+++ Algoritmo.js
@@ -17,3 +17,24 @@ Senão
 fimse
 
 Fimalgoritmo
+
+
+///////////////////////////////////
+
+Algoritmo "Verifica classificação indicativa"
+
+Var
+class_indicativa, idade_cliente: Inteiro
+
+Inicio
+Escreva("Informa sua Idade: ")
+Leia(Idade_cliente)
+Escreva("Informe a classificação indicativa do filme: ")
+Leia(class_indicativa)
+Se (idade_cliente <= class_indicativa) então
+    Escreva("Esse filme não é indicado para a sua Faixa etária")
+Senão
+    Escreva("Esse filme é indicado para a sua Faixa etária")
+fimse
+
+Fimalgoritmo
